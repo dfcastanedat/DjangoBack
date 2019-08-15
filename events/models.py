9c2a4a48c7479e9ec2caab31e5beb19cd911f112
dfcastanedat <dfcastanedat@unal.edu.co>
2019-08-15 16:42:33 -0500

    New database

--- events/models.py
+++ events/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField(max_length=100, unique=False)
     event = models.CharField(max_length=100, blank=True)
-    about = models.CharField(max_length=250, blank=True)
+    about = models.CharField(max_length=300, blank=True)
     created_at = models.DateField(auto_now_add=True)
