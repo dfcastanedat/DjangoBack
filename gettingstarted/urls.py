56da4c96ab55bb26734c05a21b4ac4d3a3f027e3
dfcastanedat <dfcastanedat@unal.edu.co>
2019-08-15 16:12:07 -0500

    xd

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,5 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
+
+from django.contrib import admin
+
+admin.autodiscover()
+
 
 urlpatterns = [
     path('', include('Events.urls')),
--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -1,17 +1,15 @@
 """
-WSGI config for Back project.
+WSGI config for gettingstarted project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Back.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
 
