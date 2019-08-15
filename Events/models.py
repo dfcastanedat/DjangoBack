246db16018e871d37798b857e5b8e45941730e1c
dfcastanedat <dfcastanedat@unal.edu.co>
2019-08-15 15:25:07 -0500

    Pipenv's shit

--- Events/models.py
+++ Events/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Event(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=False)
     event = models.CharField(max_length=100, blank=True)
     about = models.CharField(max_length=250, blank=True)
     created_at = models.DateField(auto_now_add=True)
